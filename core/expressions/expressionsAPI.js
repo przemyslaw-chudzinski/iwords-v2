3532df96aaf7a93dc6ad01f3023e8be89a286ad4
przemyslawchudzinski <przemyslaw-chudzinski@wp.pl>
2020-01-05 12:58:02 +0100

    removed import logic

--- core/expressions/expressionsAPI.js
+++ core/expressions/expressionsAPI.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const multer = require('multer');
-const csvParser = require('csv-parser');
+// const multer = require('multer');
+// const csvParser = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
 const {
@@ -16,24 +16,24 @@ const {
     countAllUserExpressions
 } = require('./expressionDAL');
 
-const fileFilter = (req, file, next) => {
-    if (file.mimetype.split('/').includes('csv')) {
-        return next(null, true);
-    }
+// const fileFilter = (req, file, next) => {
+//     if (file.mimetype.split('/').includes('csv')) {
+//         return next(null, true);
+//     }
+//
+//     return next(new Error('Wrong mimeType'), false);
+// };
 
-    return next(new Error('Wrong mimeType'), false);
-};
+// const storage = multer.diskStorage({
+//     destination(req, file, next) {
+//         next(null, './uploads/');
+//     },
+//     filename(req, file, next) {
+//         next(null, new Date().toISOString() + '_' + file.originalname);
+//     }
+// });
 
-const storage = multer.diskStorage({
-    destination(req, file, next) {
-        next(null, './uploads/');
-    },
-    filename(req, file, next) {
-        next(null, new Date().toISOString() + '_' + file.originalname);
-    }
-});
-
-const upload = multer({storage, fileFilter});
+// const upload = multer({storage, fileFilter});
 
 /* Routes */
 
@@ -101,40 +101,40 @@ router.post('/expression/:id/increment-counter', (req, res) => {
 
 });
 
-router.post('/import', upload.single('csv'), (req, res) => {
-
-    const filename = req.file.filename;
-    const results = [];
-
-    fs.createReadStream(path.resolve(__dirname, '..', '..', 'uploads', filename))
-        .pipe(csvParser())
-        .on('data', ({expression, translations, description}) => {
-            expression = expression.trim().toLowerCase();
-
-            translations = translations
-                .trim()
-                .split(',')
-                .map(t => t.trim().toLowerCase())
-                .filter(t => t && t.length && t !== '');
-
-            description = description ? description.trim() : '';
-
-            if (expression && expression.length && expression !== '' && translations && translations.length) {
-                results.push({expression, translations, description});
-            }
-        })
-        .on('end', async () => {
-            try {
-                await saveExpressions(results);
-                res.status(200);
-                await res.json({});
-            } catch (e) {
-                res.status(400);
-                await res.json({error: true});
-            }
-        });
-
-});
+// router.post('/import', upload.single('csv'), (req, res) => {
+//
+//     const filename = req.file.filename;
+//     const results = [];
+//
+//     fs.createReadStream(path.resolve(__dirname, '..', '..', 'uploads', filename))
+//         .pipe(csvParser())
+//         .on('data', ({expression, translations, description}) => {
+//             expression = expression.trim().toLowerCase();
+//
+//             translations = translations
+//                 .trim()
+//                 .split(',')
+//                 .map(t => t.trim().toLowerCase())
+//                 .filter(t => t && t.length && t !== '');
+//
+//             description = description ? description.trim() : '';
+//
+//             if (expression && expression.length && expression !== '' && translations && translations.length) {
+//                 results.push({expression, translations, description});
+//             }
+//         })
+//         .on('end', async () => {
+//             try {
+//                 await saveExpressions(results);
+//                 res.status(200);
+//                 await res.json({});
+//             } catch (e) {
+//                 res.status(400);
+//                 await res.json({error: true});
+//             }
+//         });
+//
+// });
 
 /* Statistics */
 router.get('/statistics', (req, res) => {
