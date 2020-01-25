a9063089e8a7e9e9e828243f5830425bcc147ca3
przemyslawchudzinski <przemyslaw-chudzinski@wp.pl>
2020-01-25 21:15:27 +0100

    mark which expression have notes

--- core/notes/note.js
+++ core/notes/note.js
@@ -3,8 +3,15 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String},
-    userId: {type: mongoose.Schema.Types.ObjectID, required: true},
-    expressionId: {type: mongoose.Schema.Types.ObjectID},
+    userId: {
+        type: mongoose.Schema.Types.ObjectID,
+        required: true,
+        ref: 'User'
+    },
+    expressionId: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'Expression'
+    },
     createdAt: {type: Date, default: new Date().toISOString()},
     updatedAt: {type: Date, default: new Date().toISOString()}
 });
